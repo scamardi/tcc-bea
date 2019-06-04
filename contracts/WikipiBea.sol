84d29e98752aa3f56046d90f8b3809952d7981e0
Doctor <Dovahkiin@TARDIS-3.local>
2019-06-03 21:49:31 -0300

    contratos criados

---
+++ contracts/WikipiBea.sol
@@ -0,0 +1,47 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+contract WikiBea {
+
+  struct artigo {
+  	uint id;
+  	string titulo;
+  	string corpo;
+  	address autor;
+  	uint versao;
+  }
+
+  address public owner;
+
+  constructor() public {
+    owner = msg.sender;
+  } 
+
+  // mapeando o artigo
+  mapping(uint => artigo) public artigos;
+
+  uint public artigosCount;
+
+  // inicialização
+  // function WikiBea () public {}
+
+  function adicionaArtigo (string memory _titulo, string memory _corpo) public {
+  	artigosCount ++;
+  	artigos[artigosCount] = artigo(artigosCount, _titulo, _corpo, msg.sender, 0);
+  }
+
+  function alterarArtigo (uint _id, string memory _titulo, string memory _corpo) public {
+  	// verifica se a pessoa que esta tentando alterar eh o autor
+  	require(artigos[_id].autor == msg.sender);
+
+  	// altera artigo
+  	artigos[_id].titulo = _titulo;
+  	artigos[_id].corpo = _corpo;
+
+  	// versiona
+  	artigos[_id].versao ++;
+  }
+
+  
+
+
+}
